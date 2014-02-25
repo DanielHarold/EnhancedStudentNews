78924e8fa546c07812405eba56c03f25caa83b05
Daniel Harold <DanielHarold@users.noreply.github.com>
2014-02-24 21:25:32 -0500

    Rename testview.php to view.php (web viewer) and show the latest issue by default

---
+++ src/view.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting (E_ALL);
+
+require_once ('config.php');
+
+require_once ('NewsIssue.php');
+
+if (isset($_REQUEST['issue'])) {
+	$newsIssue = new NewsIssue();
+	$newsIssue->loadFromURL(BASE_SOURCE_URL . $_REQUEST['issue']);
+} else {
+	$newsIssue = NewsIssue::getLatestIssue(isset($_REQUEST['calvinnews']));
+}
+
+echo $newsIssue->getFormattedHTML(isset($_REQUEST['asifto']));
+
+?>
