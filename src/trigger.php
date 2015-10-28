1dc72d72843b03d77407b4b8f1a2bdace3e12663
Daniel Harold <DanielHarold@users.noreply.github.com>
2015-10-27 21:34:07 -0400

    Adjustments to email trigger script:
    - Add the ability to detect and avoid resending issues that were previously sent (6-6-2014)
    - Add the ability to set a variable to easily choose a specific issue to send in the code
    - Add the url-trigger.php script which only works when called from localhost
    - Wait a few seconds between sending emails

--- src/trigger.php
+++ src/trigger.php
@@ -10,6 +10,9 @@ require_once ('functions.php');
 echo '------' . "\n";
 echo 'Triggered at ' . date(DATE_RSS) . "\n";
 
+$issue_manual_override = FALSE;
+//$issue_manual_override = 'student-news/201508/0017';
+
 // TODO put test mode back into being a constant everywhere
 
 if (!defined('NEWS_TRIGGER'))
@@ -20,7 +23,7 @@ if (!defined('NEWS_TRIGGER'))
 
 include_once ('common.php');
 
-$targets = array( 
+$targets = array(
 	// TARGET EMAIL ADDRESSES GO HERE
 //	'john@example.com',
 //	'jane@example.com',
@@ -33,28 +36,55 @@ $targets = array(
 // TODO automatically add @students.calvin.edu for Student News when no domain is specified
 //      and add @calvin.edu for Calvin News
 
-$newsIssue = NewsIssue::getLatestIssue($calvinNews);
-if (!$newsIssue) {
-	die ('Unable to find latest news issue.');
+if ( ! $issue_manual_override ) {
+	$newsIssue = NewsIssue::getLatestIssue($calvinNews);
+	if (!$newsIssue) {
+		die ('Unable to find latest news issue.');
+	}
+} else {
+	echo "Manual override for issue " . $issue_manual_override . "\n";
+	$newsIssue = new NewsIssue();
+        $newsIssue->loadFromURL(BASE_SOURCE_URL . $issue_manual_override );
 }
 
 $formattedNews = $newsIssue->getFormattedHTML(true);
 
 // TODO If any errors occurred, abort.
 
-echo 'Sending ' . $newsIssue->issueTitle . '...' . "\n";
 
-foreach ($targets as $target) {
-	$success = mail($target, $newsIssue->issueTitle, $formattedNews, "From: Enhanced Student News <" . FROM_EMAIL . ">\r\nContent-Type: text/html\r\nPrecedence: bulk");
-	echo $target . ': ' . $success . "\n";
+// Check if RSS file for this issue exists yet.
+// If so, abort to avoid sending duplicate issues.
+// (This code was added on 6-6-2014.
+//  The first time a duplicate issue was sent was on 5-5-2014.)
+if (file_exists('rss/esn-' . $newsIssue->issueNumber . '.rss')) {
+	die ('RSS file for this issue already exists. Aborting.' . "\n");
 }
 
-echo '--Completed-- at ' . date(DATE_RSS) . "\n";
 
+// Write RSS files
 echo 'Writing RSS files: ';
 writeFile('rss/esn-latest-issue.rss', $newsIssue->getRSS());
 echo '; ';
 writeFile('rss/esn-' . $newsIssue->issueNumber . '.rss', $newsIssue->getRSS());
+
+// Write HTML file for web site
+echo "\n" . 'Writing HTML file: ';
+writeFile('latest-issue.html', $newsIssue->getFormattedHTML(false));
 echo "\n\n";
 
-?>
+
+
+echo 'Sending ' . $newsIssue->issueTitle . '...' . "\n";
+
+foreach ($targets as $target) {
+	$success = mail($target, $newsIssue->issueTitle, $formattedNews, "From: Enhanced Student News <" . FROM_EMAIL . ">\r\nMIME-Version: 1.0\r\nContent-Type: text/html\r\nPrecedence: bulk");
+	echo $target . ': ' . $success . "\n";
+	@ob_flush();
+	flush();
+	
+	// wait 3.6 seconds between messages (beginning 4-14-2014; before it was 0.5 seconds)
+	usleep(3.6 * 1000000);
+	// (We would actually need to wait 3.6 seconds for a rate of 1000 emails per hour.)
+}
+
+echo '--Completed-- at ' . date(DATE_RSS) . "\n";
---
+++ src/url-trigger.php
@@ -0,0 +1,14 @@
+<?php
+
+if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1' && $_SERVER['REMOTE_ADDR'] !== $_SERVER['SERVER_ADDR']) {
+	die ('Forbidden');
+} else {
+	define ('NEWS_TRIGGER', true);
+	
+	$calvinNews = false;
+	$testNews = false;
+	
+	include ('trigger.php');
+}
+
+?>
