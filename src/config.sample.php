c4e9731510b88f1e4749b885f9611715a0650213
DanielHarold <DanielHarold@users.noreply.github.com>
2016-04-30 15:15:19 -0400

    Rearranged settings in config file and added comments for documentation

--- src/config.sample.php
+++ src/config.sample.php
@@ -2,15 +2,23 @@
 
 // Copy this file to "config.php" and fill in values for these settings.
 
+// Where to get the news from
 define ('BASE_SOURCE_URL', 'http://www.example.com/archive/');
 
-define ('FROM_EMAIL', 'enhanced-student-news');
-define ('DEBUG_EMAIL', 'test@example.com');
+// Relative paths -- used by "previous issue" and "next issue" links
+define ('CALVIN_NEWS_BASE_VIEW_PATH', 'view.php?issue=calvin-news/');
+define ('STUDENT_NEWS_BASE_VIEW_PATH', 'view.php?issue=student-news/');
 
+// Email addresses used on the web site for those who wish to subscribe
+// to Enhanced Calvin News or Enhanced Student News
 define ('CALVIN_NEWS_SUBSCRIBE_EMAIL', '<span style="color: #333355">ecn</span>[a<!-- not putting the actual symbol here to discourage automated spammers -->t]<!-- somewhere near --><span style="color: #333355">example.com</span>');
 define ('STUDENT_NEWS_SUBSCRIBE_EMAIL', '<span style="color: #333355">esn</span>[a<!-- not putting the actual symbol here to discourage automated spammers -->t]<!-- somewhere near --><span style="color: #333355">example.com</span>');
 
+// Name of a person for people to contact with questions
 define ('CONTACT_NAME', 'John Doe');
 
-define ('CALVIN_NEWS_BASE_VIEW_PATH', 'view.php?issue=calvin-news/');
-define ('STUDENT_NEWS_BASE_VIEW_PATH', 'view.php?issue=student-news/');
+// Email address to send issues from
+define ('FROM_EMAIL', 'enhanced-student-news');
+
+// Email address to send debugging messages to
+define ('DEBUG_EMAIL', 'test@example.com');
